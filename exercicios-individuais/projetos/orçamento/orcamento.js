31b71e0a158e368e656848c7a7e67f5d73d4b52b
Victor Sales <vitorsaleskikao@gmail.com>
2025-10-11 09:26:23 -0300

    .

--- exercicios-individuais/projetos/orçamento/orcamento.js
+++ exercicios-individuais/projetos/orçamento/orcamento.js
@@ -1,52 +1,30 @@
-{/* <div class="text-center">
-                    <h3>Resumo do Orçamento</h3>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item d-flex justify-content-between">
-                            <span>Subtotal (Páginas):</span>
-                            <span id="resumo-subtotal">R$ 0,00</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between">
-                            <span>Adicional de Design:</span>
-                            <span id="resumo-adicional">R$ 0,00</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between text-warning">
-                            <span>Taxa de Urgência:</span>
-                            <span id="resumo-urgencia">+ R$ 0,00</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between text-danger">
-                            <span>Desconto Aplicado:</span>
-                            <span id="resumo-desconto">- R$ 0,00</span>
-                        </li>
-                        <li class="list-group-item d-flex justify-content-between fs-4 fw-bold border-top pt-3">
-                            <span>TOTAL:</span>
-                            <span id="resumo-total">R$ 0,00</span>
-                        </li>
-                    </ul>
-                </div> */}
-//     Taxa de Urgência:
-//     - Se o prazo for menor que 5 dias: 10 % sobre o valor base(páginas + design).
-//    - Se o prazo for menor que 15 dias: 5 % sobre o valor base(páginas + design). 
-//    - Se for 15 dias ou mais, a taxa é zero.
-
 
 const PRECO_POR_PAGINA = 500;
 const PRECO_DESIGN_ADICIONAL = 1000
 
+
 document.querySelector(".seu-nome").textContent = "Victor"
+
+
+// VALORES DOS INPUTS
 const inputPaginas = document.querySelector("#qtd-paginas") // QUANTIDADE DE PÁGINAS INSERIDAS PELO CLIENTE -> $500
 const inputPrazo = document.querySelector("#prazo-entrega")
 const inputDesconto = document.querySelector("#desconto"); // DESCONTO
+const inputMensalidade = document.querySelector("#mensalidade")
 const checkBoxDesign = document.querySelector("#inclui-design") // PRAZO DE URGÊNCIA
+
+
+// EXIBIÇÃO
 const resumoSubTotal = document.querySelector("#resumo-subtotal")
 const resumoAdicional = document.querySelector("#resumo-adicional")
 const resumoUrgencia = document.querySelector("#resumo-urgencia")
 const resumoDesconto = document.querySelector("#resumo-desconto")
 const resumoTotal = document.querySelector("#resumo-total")
+const resumoMensalidade = document.querySelector("#resumo-mensalidade")
 
-// FORMULÁRIO DE INPUT
+// FORMULÁRIO
 const form = document.querySelector("#form")
-form.addEventListener("input", () => { console.log("Altrado") })
-
+form.addEventListener("input", () => atualizarOrcamento())
 
 const calcularSubTotal = (quantidade) => quantidade * PRECO_POR_PAGINA
 
@@ -68,18 +46,43 @@ function calcularTaxaDeUrgencia(valor, prazo) {
 function atualizarOrcamento() {
 
     const qtdPagina = Number(inputPaginas.value);
-    const porcentagemDesconto = Nmber(inputDesconto.value);
+    const porcentagemDesconto = Number(inputDesconto.value);
     const prazo = Number(inputPrazo.value);
     const designIncluido = checkBoxDesign.checked
+
 
     const subtotal = calcularSubTotal(qtdPagina)
 
     const adicionalDesign = designIncluido ? PRECO_DESIGN_ADICIONAL : 0
 
-    // const calcularValorDesconto = (subtotal, porcentagemDesconto)
+    const taxaUrgencia = calcularTaxaDeUrgencia(subtotal + adicionalDesign, prazo)
 
 
+    const valorDesconto = calcularValorDesconto(subtotal + adicionalDesign + taxaUrgencia, porcentagemDesconto)
+
+
+    const total = (subtotal + adicionalDesign + taxaUrgencia) - valorDesconto
+
+
+    const formatarValor = valor => valor.toLocaleString('pt-BR',
+        {
+            style: "currency",
+            currency: "BRL"
+        }
+    )
+
+    resumoSubTotal.textContent = formatarValor(subtotal)
+    resumoAdicional.textContent = formatarValor(adicionalDesign)
+    resumoUrgencia.textContent = formatarValor(taxaUrgencia)
+    resumoDesconto.textContent = formatarValor(valorDesconto)
+    resumoTotal.textContent = formatarValor(total)
+
+    resumoMensalidade.textContent = Number(inputMensalidade.value)
 }
+
+
+document.addEventListener("DOMContentLoaded", atualizarOrcamento())
+
 
 
 
